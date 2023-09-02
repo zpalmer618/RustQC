aeb6b7f83c1ceaca857089739973a55d224a233b
Zach <zpalmer618@gmail.com>
2023-09-02 10:00:02 -0500

    Introduced structs and more refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,47 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-fn main() {
-    let (contents, charge, x, y, z) = load_geom();
-    dbg!(charge, x, y, z);
-    println!("{}", contents);
+#[derive(Debug)]
+struct Atom {
+    charge: usize,
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+#[derive(Debug)]
+struct Molecule {
+    atoms: Vec<Atom>,
 }
 
 /// Load a geometry from geom.txt and split it into appropriate vectors
-fn load_geom() -> (String, Vec<usize>, Vec<f64>, Vec<f64>, Vec<f64>) {
+fn load_geom() -> (String, Molecule) {
     let file = File::open("testfiles/geom.txt");
     let mut contents = String::new();
-    file.expect("failed to open file").read_to_string(&mut contents).unwrap();
-    let mut charge = Vec::new();
-    let mut x = Vec::new();
-    let mut y = Vec::new();
-    let mut z = Vec::new();
+    file.expect("failed to open file")
+        .read_to_string(&mut contents)
+        .unwrap();
+    let mut atoms = Vec::new();
     for line in contents.lines() {
         let line: Vec<_> = line.split_whitespace().collect();
         if line.len() > 1 {
-            charge.push(line[0].parse::<usize>().unwrap());
-            x.push(line[1].parse::<f64>().unwrap());
-            y.push(line[2].parse::<f64>().unwrap());
-            z.push(line[3].parse::<f64>().unwrap());
+            let charge = line[0].parse::<usize>().unwrap();
+            let x = line[1].parse::<f64>().unwrap();
+            let y = line[2].parse::<f64>().unwrap();
+            let z = line[3].parse::<f64>().unwrap();
+            let atom = Atom { charge, x, y, z };
+            atoms.push(atom)
         }
     }
-    (contents, charge, x, y, z)
+    (contents, Molecule { atoms })
+}
+
+// fn bond_length() {
+//     for i in
+// }
+
+fn main() {
+    let (contents, mol) = load_geom();
+    dbg!(mol);
+    println!("{}", contents);
 }
